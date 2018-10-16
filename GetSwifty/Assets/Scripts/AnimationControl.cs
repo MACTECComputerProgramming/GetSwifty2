5320d0c5b3a0bbdf2eee5da0481fd874b99d404b
Grey Spencer <19grspencer@macon.k12.mo.us>
2018-10-16 14:03:41 -0500

    Dat boi, plus beginning animation in the works

---
+++ GetSwifty/Assets/Scripts/AnimationControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationControl : MonoBehaviour {
+
+    public Sprite jumpSprite;
+    public Sprite restSprite;
+    private bool isGrounded;
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
+    public float checkRadius;
+    private SpriteRenderer sr;
+
+    // Use this for initialization
+    void Start () {
+        sr = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (isGrounded)
+        {
+            sr.sprite = restSprite;
+        }
+        if (!isGrounded)
+        {
+            sr.sprite = jumpSprite;
+        }
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+    }
+}
