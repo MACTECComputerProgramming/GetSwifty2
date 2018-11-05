0d4d43c99e2f037d792d14313ed76b7422fcbf6b
AndrewBPayne <20abpayne@macon.k12.mo.us>
2018-11-05 14:02:39 -0600

    Finnish the invincibility script.

--- GetSwifty/Assets/Scripts/CharacterHealth.cs
+++ GetSwifty/Assets/Scripts/CharacterHealth.cs
@@ -7,24 +7,33 @@ using UnityEngine.SceneManagement;
 public class CharacterHealth : MonoBehaviour {
 
 
-    public int playerHealthMax;
-    public int playerHealthCurrent;
+[SerializeField]
+    private int playerHealthCurrent = 100;
     public Slider healthBar;
     public Scene winScene;
     public Scene loseScene;
     public Scene BossBattle;
-    
+    public Renderer rend;
+    Color c;
+    public int invincibilityTime;
+    public bool invinc;
 
 	
 	void Start () {
-        
-        
+        rend = GetComponent<Renderer>();
+        c = rend.material.color;
+        invinc = false;
+        playerHealthCurrent = 100;
     }    
     void Update() {
+
+        
+        
         if (playerHealthCurrent <= 0)
         {
             SceneManager.LoadScene(4);
             ScoreScript.scoreValue = 0;
+            Physics2D.IgnoreLayerCollision(9, 10, false);
         }
         if (transform.position.y < -20)
         {
@@ -41,23 +50,32 @@ public class CharacterHealth : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Enemy")
+        if (collision.gameObject.tag == "Enemy" && invinc == false)
         {
             StartCoroutine(Damage());
-            
+            Physics2D.IgnoreLayerCollision(9, 10, true);
         }
         if (collision.gameObject.tag.Equals("Flag"))
         {
             SceneManager.LoadScene(5);
             
         }
-
+        
+        
     }
     IEnumerator Damage()
     {
+        invinc = true;
+        c.a = 0.5f;
+        rend.material.color = c;
+        
         playerHealthCurrent -= 25;
         healthBar.value = CalculateHealth();
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(invincibilityTime);
+        c.a = 1f;
+        rend.material.color = c;
+        invinc = false;
+        Physics2D.IgnoreLayerCollision(9, 10, false);
         StopCoroutine(Damage());
     }
     public float CalculateHealth()
